cdfefb319f9ecd48b025fceb018716b910ac7147
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-02 01:32:17 +0330

    Ready to put unveiling pics and texts

--- viradaco/virada/views.py
+++ viradaco/virada/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def home(request):
+	contents=Content.objects.all()
+
+	return render(request,'Virada/home.html',{'contents':contents})
