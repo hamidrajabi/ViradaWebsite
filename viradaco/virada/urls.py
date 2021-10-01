79f586a95e4071f240cdcc84a598ad7c5305bc07
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-02 01:34:35 +0330

    add extra files

---
+++ viradaco/virada/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from django.conf.urls.static import static
+from django.conf import settings 
+from . import views
+
+urlpatterns=[
+	#base url
+	path('',views.home,name="Home"),
+	]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
