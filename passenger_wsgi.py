4a286f9fd34c1797a19e29c703eab4ea29a466a3
Hamid Rajabi <hamid1rajabi@gmail.com>
2021-10-02 23:54:59 +0330

    Changes from the Cpanel

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from viradaco.wsgi import application
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
---
+++ virada/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Content(models.Model):
+	priority=models.IntegerField(null=True)
+	title=models.CharField(max_length=200)
+	image=models.ImageField()
+	text=models.TextField()
+
+	def imageURL(self):
+		try:
+			url=self.image.url
+		except:
+			url=''
+		return url
+
+	def __str__(self):
+		return str(self.title)
