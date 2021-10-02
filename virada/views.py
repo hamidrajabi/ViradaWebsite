1e80285a76129f8c075b85bb217679c7fa4263f5
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-03 03:03:15 +0330

    added

--- virada/views.py
+++ virada/views.py
@@ -3,5 +3,8 @@ from .models import *
 # Create your views here.
 def home(request):
 	contents=Content.objects.all()
-	logo=Picture.objects.get(title='logo')
+	try:
+		logo=Picture.objects.get(title='logo')
+	except:
+		logo=''
 	return render(request,'Virada/home.html',{'contents':contents,'logo':logo})
