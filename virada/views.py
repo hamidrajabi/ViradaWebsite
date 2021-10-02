99e46b2e669bd2f53a9f1e32847f97545bf2f8ca
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-03 02:55:00 +0330

    dynamic logo

--- virada/views.py
+++ virada/views.py
@@ -3,5 +3,5 @@ from .models import *
 # Create your views here.
 def home(request):
 	contents=Content.objects.all()
-	logo=Picture.objects.get(title=logo)
+	logo=Picture.objects.get(title='logo')
 	return render(request,'Virada/home.html',{'contents':contents,'logo':logo})
