cfadccfd313b76b0a0982eaa3e164ee389a5ee86
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-03 03:54:25 +0330

    Order

--- virada/models.py
+++ virada/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Customer(models.Model):
+	name=models.CharField(max_length=200)
+	phone_number = PhoneNumberField()
+	email=models.EmailField(null=True,blank=True)
+
+	def __str__(self):
+		return str(self.name)
 
 class Content(models.Model):
 	priority=models.IntegerField(null=True)
@@ -36,5 +45,10 @@ class Picture(models.Model):
 class order(models.Model):
 	customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
 	address=models.CharField(max_length=1000)
-	
+	date_added=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.customer)
+		
+
 
--- virada/views.py
+++ virada/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
 # Create your views here.
 def home(request):
 	contents=Content.objects.all()
@@ -10,3 +11,8 @@ def home(request):
 		logo=""
 
 	return render(request,'Virada/home.html',{'contents':contents,'logo':logo})
+
+def orderPage(request):
+	form=OrderForm
+
+	return render(request,'Virada/order.html',{'form':form})
