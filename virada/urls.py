7ab8c7f30de0b504f223af60e98e0689eb44e6f6
hamidrajabi <hamid1rajabi@gmail.com>
2021-10-03 04:01:38 +0330

    slash

--- virada/urls.py
+++ virada/urls.py
@@ -9,7 +9,7 @@ from . import views
 urlpatterns=[
 	#base url
 	path('',views.home,name="Home"),
-	path('/order',views.orderPage,name="Order"),
+	path('order/',views.orderPage,name="Order"),
 	]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
